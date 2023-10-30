5427c38a9db525682595bec65314648170e1cbca
Juan Fangano <juanyfanga@gmail.com>
2023-10-30 20:07:42 -0300

    Beginning of Reloading

--- Sist-HollowEmblem/Assets/_Scripts/Concrete Strategies/PlayerAttack/PlayerShoot.cs
+++ Sist-HollowEmblem/Assets/_Scripts/Concrete Strategies/PlayerAttack/PlayerShoot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,10 @@ public class PlayerShoot : MonoBehaviour, IPlayerAttack
     [SerializeField] private Transform _attackStartPosition;
     [SerializeField] private float _damage;
     [SerializeField] private bool _isAiming;
+    [SerializeField] private int _bulletsRemaining; // Es SerializeField para verlo en el Inspector, después lo borro
+    private int _maxBullets = 10;
     private Animator _animator;
+    private PlayerInventory _playerInventory;
 
     #region IPlayerAttack properties
     public PlayerMovementController PlayerMovementController => throw new System.NotImplementedException();
@@ -28,6 +32,7 @@ public class PlayerShoot : MonoBehaviour, IPlayerAttack
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _playerInventory = GetComponent<PlayerInventory>();
     }
 
     public void Attack(int direction)
@@ -48,5 +53,15 @@ public class PlayerShoot : MonoBehaviour, IPlayerAttack
         _isAiming = isTrue;
         _animator.SetTrigger("AimFront");
         _animator.SetBool("Aiming", _isAiming);
+    }
+
+    public void Reload()
+    {
+        _bulletsRemaining += _playerInventory.BulletsInInventory;
+
+        if (_bulletsRemaining >= _maxBullets)
+        {
+            _bulletsRemaining = _maxBullets;
+        }
     }
 }
