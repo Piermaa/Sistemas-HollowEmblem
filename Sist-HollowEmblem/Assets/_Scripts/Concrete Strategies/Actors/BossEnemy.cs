ff79f1d1933285e712fa6b5fcea75aee9bc08c31
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-10-30 20:15:16 -0300

    Update BossEnemy.cs

--- Sist-HollowEmblem/Assets/_Scripts/Concrete Strategies/Actors/BossEnemy.cs
+++ Sist-HollowEmblem/Assets/_Scripts/Concrete Strategies/Actors/BossEnemy.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class BossPhase
+{
+    public int HealthUmbral;
+    public int AttackCooldown;
+}
+
 public class BossEnemy : Enemy
 {
     #region Serialized Variables
 
     [SerializeField] private List<BossPhase> _bossPhases;
-    [SerializeField] private int _phase=0;
+    [SerializeField] private int _phase=-1;
 
     #endregion
 
@@ -16,16 +22,30 @@ public class BossEnemy : Enemy
 
     private BossPhase _currentPhase=default;
     private const string PHASE_INTEGER = "Phase";
-
+    private const string ATTACK_ANIMATOR_PARAMETER = "Attack";
+    private const string ATTACK_TRIGGER = "Attack";
+    private float _bossAttackCooldown;
     #endregion
 
     #region Unity Callbacks
 
     private void Start()
     {
-        ChangePhase(_phase);
+        ChangePhase();
+        _bossAttackCooldown = _enemyStats.AttackCooldown;
     }
+    
+    protected override void Update()
+    {
+        base.Update();
 
+        if (_attackCooldownTimer<=0)
+        {
+            _animator.SetTrigger(ATTACK_ANIMATOR_PARAMETER);
+            _attackCooldownTimer = _bossAttackCooldown;
+        }
+    }
+    
     #endregion
 
     #region Class Methods
@@ -35,20 +55,22 @@ public class BossEnemy : Enemy
         return CurrentHealth < _currentPhase.HealthUmbral;
     }
 
-    private void ChangePhase(int index)
+    private void ChangePhase()
     {
-        _attackTimer = 0;
+        _phase++;
+        _attackCooldownTimer = 0;
         _animator.ResetTrigger(ATTACK_TRIGGER);
-        if (index>0)
+        if (_phase>0)
         {
-            _currentPhase.Effects.SetActive(false);
+       //     _currentPhase.Effects.SetActive(false);
         }
-        _animator.SetInteger(PHASE_INTEGER,_phase);
-        _currentPhase = _bossPhases[index];
-        _currentPhase.Effects.SetActive(true);
-        
-        _cmdEnemyAttack = new(_currentPhase.PhaseEnemyAttack,_enemyStatsValues,attackOrigin);
-        _agentController.SetAgent(_currentPhase.PhaseAgent);
+        _animator.SetInteger(PHASE_INTEGER, _phase);
+        _currentPhase = _bossPhases[_phase];
+        _bossAttackCooldown = _bossPhases[_phase].AttackCooldown;
+        //   _currentPhase.Effects.SetActive(true);
+
+        //    _cmdEnemyAttack = new(_currentPhase.PhaseEnemyAttack,_enemyStatsValues,attackOrigin);
+        //    _agentController.SetAgent(_currentPhase.PhaseAgent);
     }
 
     #endregion
@@ -60,8 +82,7 @@ public class BossEnemy : Enemy
         base.TakeDamage(damage);
         if (CheckPhaseChange())
         {
-            _phase++;
-            ChangePhase(_phase);
+            ChangePhase();
         }
     }
 
